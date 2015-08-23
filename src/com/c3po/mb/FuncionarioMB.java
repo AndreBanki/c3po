3c2386ac6c729e2e434cb13013a1e79e5ef39c33
André Luiz Banki <banki@altoqi.com.br>
2015-08-22 21:46:10 -0300

    Correções na página Cliente e Funcionário

--- src/com/c3po/mb/FuncionarioMB.java
+++ src/com/c3po/mb/FuncionarioMB.java
@@ -62,7 +62,7 @@ public class FuncionarioMB {
 		}
 		else {
 			dao.salvar(funcionarioEmEdicao);
-                        atualizaListaFuncionariosParaExibicao();
+            atualizaListaFuncionariosParaExibicao();
 		}
 	}
 	
